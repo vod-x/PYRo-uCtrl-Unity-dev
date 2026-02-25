484c58fb60bf26704fef4a0e1aa4ddd9e4ac5800
vod-x <vod_x@outlook.com>
2026-02-25 22:10:40 +0800

    暂存

--- PYRo/Module/Chassis/Wheel_Legged/pyro_wl_chassis.cpp
+++ PYRo/Module/Chassis/Wheel_Legged/pyro_wl_chassis.cpp
@@ -13,38 +13,43 @@
 
  namespace pyro
  {
-wl_chassis_t::wl_chassis_t(cfg_t *cfg) : module_base_t("wl_chassis")
+wl_chassis_t::wl_chassis_t() : module_base_t("wl_chassis")
+{
+}
+
+void wl_chassis_t::_init()
 {
     /* Initialize kinematic solver with given coefficients. */
-    _kinematic_solver.init(&cfg->phi_k, &cfg->polar_k, 
-                                                   &cfg->vmc_k);
+    _kinematic_solver.init(&_config.phi_k, &_config.polar_k, 
+                                                   &_config.vmc_k);
     /* Save LQR coefficients */
-    memcpy(_lqr_cof, cfg->lqr_coef, sizeof(float) * 36);
+    memcpy(_lqr_cof, _config.lqr_coef, sizeof(float) * 36);
 
     /* Save wheel radius and reduction ratio */
-    _wheel_radius = cfg->wheel_radius;
-    _reduction_ratio = cfg->reduction_ratio;
+    _wheel_radius = _config.wheel_radius;
+    _reduction_ratio = _config.reduction_ratio;
 
     /* Initialize joint motor driver */
     for(uint8_t i = 0; i < 4; i++)
     {
-        _motor_drv[i] = new dm_motor_drv_t(cfg->joint_motor_cfg[i].tx_id,
-                                           cfg->joint_motor_cfg[i].rx_id,
-                                           cfg->joint_motor_cfg[i].can);
-        _motor_offset[i] = cfg->joint_motor_cfg[i].offset_angle;
-        _motor_drv[i]->set_rotate_range(cfg->rotate_min, 
-                                                  cfg->rotate_max);
-        _motor_drv[i]->set_position_range(cfg->position_min,
-                                                  cfg->position_max);
-        _motor_drv[i]->set_torque_range(cfg->torque_min,
-                                                  cfg->torque_max);
+        _motor_drv[i] = new dm_motor_drv_t(_config.joint_motor_cfg[i].tx_id,
+                                           _config.joint_motor_cfg[i].rx_id,
+                                           _config.joint_motor_cfg[i].can);
+        _motor_offset[i] = _config.joint_motor_cfg[i].offset_angle;
+        _motor_drv[i]->set_rotate_range(_config.rotate_min, 
+                                                  _config.rotate_max);
+        _motor_drv[i]->set_position_range(_config.position_min,
+                                                  _config.position_max);
+        _motor_drv[i]->set_torque_range(_config.torque_min,
+                                                  _config.torque_max);
     }
     /* Initialize wheel motor driver */
     for(uint8_t i = 0; i < 2; i++)
     {
         _wheel_drv[i] = 
-               new dji_m3508_motor_drv_t(cfg->wheel_motor_cfg[i].tx_id,
-                                     cfg->wheel_motor_cfg[i].can);
+               new dji_m3508_motor_drv_t(_config.wheel_motor_cfg[i].tx_id,
+                                     _config.wheel_motor_cfg[i].can);
     }
+
 }
-}
+ }
--- PYRo/Module/Chassis/Wheel_Legged/pyro_wl_chassis.h
+++ PYRo/Module/Chassis/Wheel_Legged/pyro_wl_chassis.h
@@ -18,6 +18,60 @@
 
 namespace pyro
 {
+/* DM joint motor configuration structure. */
+struct wl_dm_motor_cfg_t
+{
+    /* can tx id of motor*/
+    uint8_t tx_id;
+    /* can rx id of motor*/
+    uint8_t rx_id;
+    /* Which CAN bus the motor is connected to */
+    can_hub_t::which_can can;
+    /* Offset angle to eliminate the bias of installation(rad)*/
+    float offset_angle;
+};
+
+/* DJI 3508 configuration structure.*/
+struct wl_dji_motor_cfg_t
+{
+    /* can tx id of motor*/
+    dji_motor_tx_frame_t::register_id_t tx_id;
+    /* Which CAN bus the motor is connected to */
+    can_hub_t::which_can can;
+};
+
+/* Configuration structure for wheel-legged chassis, every parameter
+   should be set here */
+struct wl_chassis_cfg_t
+{
+    /* Kinematic coefficients for the chassis. */
+    wheel_legged_kin_t::phi_k_t phi_k;
+    wheel_legged_kin_t::polar_k_t polar_k;
+    wheel_legged_kin_t::vmc_k_t vmc_k;
+
+    /* Joint motor configuration. The order of array is: front-right,
+       rear-right, front-left, rear-left */
+    wl_dm_motor_cfg_t joint_motor_cfg[4];
+    /* Wheel motor configuration. The order of array is: right wheel, left
+       wheel */
+    wl_dji_motor_cfg_t wheel_motor_cfg[2];
+    /* LQR coefficients for the chassis control. 2 raw x 6 column, 12 values
+       in total. Every value has 3 coefficients.*/
+    float lqr_coef[36];
+    /* Wheel radius(m)*/
+    float wheel_radius;
+    /* Reduction ratio of the motor */
+    float reduction_ratio;
+    /* Rotation range of the motor(rad/s)*/
+    float rotate_min;
+    float rotate_max;
+    /* Position range of the motor(rad)*/
+    float position_min;
+    float position_max;
+    /* Torque range of the motor(Nm)*/
+    float torque_min;
+    float torque_max;
+};
 //command structure for wheel-legged chassis
 struct wl_cmd_t final : public cmd_base_t
 {
@@ -40,70 +94,14 @@ struct wl_cmd_t final : public cmd_base_t
     }
 };
 
-class wl_chassis_t final : public module_base_t<wl_chassis_t, wl_cmd_t>
+class wl_chassis_t final : public module_base_t<wl_chassis_t, wl_cmd_t, wl_chassis_cfg_t>
 {
-    friend class module_base_t<wl_chassis_t, wl_cmd_t>;
+   friend class module_base_t<wl_chassis_t, wl_cmd_t, wl_chassis_cfg_t>;
 
 public:
+   
     wl_chassis_t(const wl_chassis_t &)            = delete;
     wl_chassis_t &operator=(const wl_chassis_t &) = delete;
-
-    /* DM joint motor configuration structure. */
-    struct dm_motor_cfg_t
-    {
-        /* can tx id of motor*/
-        uint8_t tx_id;
-        /* can rx id of motor*/
-        uint8_t rx_id;
-        /* Which CAN bus the motor is connected to */
-        can_hub_t::which_can can;
-        /* Offset angle to eliminate the bias of installation(rad)*/
-        float offset_angle;
-    };
-
-    /* DJI 3508 configuration structure.*/
-    struct dji_motor_cfg_t
-    {
-        /* can tx id of motor*/
-        dji_motor_tx_frame_t::register_id_t tx_id;
-        /* Which CAN bus the motor is connected to */
-        can_hub_t::which_can can;
-    };
-
-    /* Configuration structure for wheel-legged chassis, every parameter 
-       should be set here */
-    struct cfg_t
-    {
-
-        /* Kinematic coefficients for the chassis. */
-        wheel_legged_kin_t::phi_k_t phi_k;
-        wheel_legged_kin_t::polar_k_t polar_k;
-        wheel_legged_kin_t::vmc_k_t vmc_k;
-
-        /* Joint motor configuration. The order of array is: front-right, 
-           rear-right, front-left, rear-left */
-        dm_motor_cfg_t joint_motor_cfg[4];
-        /* Wheel motor configuration. The order of array is: right wheel, left
-           wheel */
-        dji_motor_cfg_t wheel_motor_cfg[2];
-        /* LQR coefficients for the chassis control. 2 raw x 6 column, 12 values
-           in total. Every value has 3 coefficients.*/
-        float lqr_coef[36];
-        /* Wheel radius(m)*/
-        float wheel_radius;
-        /* Reduction ratio of the motor */
-        float reduction_ratio;
-        /* Rotation range of the motor(rad/s)*/
-        float rotate_min;
-        float rotate_max;
-        /* Position range of the motor(rad)*/
-        float position_min;
-        float position_max;
-        /* Torque range of the motor(Nm)*/
-        float torque_min;
-        float torque_max;
-
-    };
 
 private:
     /**
@@ -113,7 +111,7 @@ private:
     *  @param {cfg_t*} cfg: configuration structure pointer, all parameters for 
        chassis should be set in this structure.
      */
-    wl_chassis_t(cfg_t *cfg);
+    wl_chassis_t();
     ~wl_chassis_t() override = default;
 
     /* base interface define */
